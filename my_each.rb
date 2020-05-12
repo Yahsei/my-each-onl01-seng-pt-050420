f2cb50332e17443e8ef0458408984eef38e85df6
Jesse Summer-Lavigne <jessesl22@gmail.com>
2020-05-12 02:24:08 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -4,13 +4,12 @@ def my_each(array)
   i = 0
 while i < array.length 
   yield(array[i])
-  my_each(list) do |i|
   i = i + 1
   end
 end
 end
 
-my_each(lnst) {|i| puts "So I think #{i} is a piece of shit"}
+my_each(list) {|i| puts "So I think #{i} is a piece of shit"}
 
 def hello(array)
   i = 0
