b32e1519d33529338791f60fa8ea96a817b81b5e
Jesse Summer-Lavigne <jessesl22@gmail.com>
2020-05-12 02:34:32 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -4,7 +4,6 @@ peeps = ['arel', 'jon', 'logan', 'spencer']
 def my_each(array)
   i = 0
   while i < array.length
-# The "while" method will always return a value of => nil"
     yield(array[i])
     i = i + 1
   end
@@ -12,7 +11,4 @@ end
 
 my_each(peeps) {|i| return "Don't touch my #{i}!!!"}
 
-# Use the collection method 
-# To make the *return-value* the answer you want
-# instead of "=> nil"
 
